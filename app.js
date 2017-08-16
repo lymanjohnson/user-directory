f28994d15312760f2a867d4645f434c316d7294c
lymanjohnson <lyman.johnson@gmail.com>
2017-08-16 14:47:52 -0400

    Styling...

--- app.js
+++ app.js
@@ -9,35 +9,27 @@ app.engine("mustache", mustacheExpress());
 app.set("views", "./views")
 app.set("view engine", "mustache")
 
+app.use(express.static(__dirname + '/public'));
 
-//Listening on root
 app.get("/", function (req, res) {
 		res.send(dataFile);
 })
 
-app.get("/directory/", function (req, res) {
+app.get("/users/", function (req, res) {
 		res.render('directory',dataFile);
 })
 
-app.get("/user/:username", function (req, res) {
+app.get("/users/:username", function (req, res) {
 
   let thisUser = dataFile.users.filter(function( obj ) {
     return obj.username == req.params.username;
-
   });
 
-  // res.send(typeof dataFile);
-  // res.send(dataFile.users);
-
-		// res.send(thisUser[0]);
     res.render('user',thisUser[0])
 })
+
+
 
 app.listen(3000, function () {
 	  console.log("Successfully started express application!");
 })
-
-//
-// app.get('/user/:id', function(request, response) {
-//   response.send('user ' + request.params.id);
-// });
